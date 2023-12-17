663ba35eb8da039c445b5fc51d6069f1a8b29f87
Dolma <dolma1918@gmail.com>
2023-12-17 17:56:08 +0545

    feat: Added icons and formatted date

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,10 +1,26 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
+
+final formatter = DateFormat.yMd();
 
 const uuid = Uuid();
 
-enum Category { food, travel, leisure, work }   //enum has list of predefined values
+enum Category {
+  food,
+  travel,
+  leisure,
+  work
+} //enum has list of predefined values
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
 
 class Expense {
   Expense({
@@ -18,4 +34,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category; //leissure expense (category:'leisure')
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
