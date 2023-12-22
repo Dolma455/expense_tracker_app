b0983998a5a114187fce024988077b1dfbe18430
Dolma <dolma1918@gmail.com>
2023-12-22 18:59:00 +0545

    feat: Closing the modal

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -36,12 +36,13 @@ class _NewExpenseState extends State<NewExpense> {
           ),
           Row(
             children: [
-              TextButton(onPressed: () {}, child: const Text('Cancel')),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancel')),
               ElevatedButton(
-                onPressed: () {
-                  print(_titleController.text);
-                  print(_amountController);
-                },
+                onPressed: () {},
                 child: const Text("Save"),
               )
             ],
